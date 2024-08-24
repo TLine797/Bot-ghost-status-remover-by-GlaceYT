36fab896f129eab9cd680104cffc171ffc656645
T Line <124744173+TLine797@users.noreply.github.com>
2024-08-24 14:03:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Made By TLine","Watching TLineFN on Twitch"];
 
 
 let currentIndex = 0;
